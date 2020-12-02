dc7b8020803226537342c60b3e82ddb6ebc365f3
imagine77 <384509364@qq.com>
2020-12-02 22:22:16 +0800

    1

---
+++ shiroo/src/test/java/com/test/shiro/AuthenticatorTest.java
@@ -0,0 +1,48 @@
+package com.test.shiro;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.Factory;
+import org.apache.shiro.util.ThreadContext;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AuthenticatorTest {
+    @Test
+    public void testAtLeastTwoStrategyWithSuccess() {
+        login("classpath:shiro-authenticator-atLeastTwo-success.ini");
+        Subject subject = SecurityUtils.getSubject();
+
+        //得到一个身份集合，因为myRealm1和myRealm4返回的身份一样所以输出时只返回一个
+        PrincipalCollection principalCollection = subject.getPrincipals();
+        Assert.assertEquals(2, principalCollection.asList().size());
+        System.out.println(principalCollection.asList());
+    }
+
+    private void login(String configFile) {
+        //1、获取SecurityManager工厂，此处使用Ini配置文件初始化SecurityManager
+        Factory<SecurityManager> factory =
+                new IniSecurityManagerFactory(configFile);
+
+        //2、得到SecurityManager实例 并绑定给SecurityUtils
+        org.apache.shiro.mgt.SecurityManager securityManager = factory.getInstance();
+        SecurityUtils.setSecurityManager(securityManager);
+
+        //3、得到Subject及创建用户名/密码身份验证Token（即用户身份/凭证）
+        Subject subject = SecurityUtils.getSubject();
+        UsernamePasswordToken token = new UsernamePasswordToken("zhang", "123");
+
+        subject.login(token);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        ThreadContext.unbindSubject();//退出时请解除绑定Subject到线程 否则对下次测试造成影响
+    }
+
+}
